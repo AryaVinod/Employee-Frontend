d0ff3ad9c92eb01449d2f54d9ac529abd4732c60
AryaVinod <aryavinod2001@gmail.com>
2023-08-11 10:34:17 +0530

    Updated Employee

--- src/components/Sub-header/Sub-header.tsx
+++ src/components/Sub-header/Sub-header.tsx
@@ -9,6 +9,12 @@ const SubHeader: FC<SubHPropTypes> = (props) => {
   return (
     <div className='subheader-container'>
       <div className='subh-text'>{props.label}</div>
+      <button className='create-employee-button'>
+        <div className='add-blue-plus'>
+          <img className='add-white-plus' src='assets/img/plus.png' />
+        </div>
+        <div className='add-button-text'>Create Employee</div>
+      </button>
     </div>
   );
 };
