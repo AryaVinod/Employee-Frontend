aa1b4e1d12bf01f7c867e22b7994d13ba4e9042e
AryaVinod <aryavinod2001@gmail.com>
2023-08-14 10:12:53 +0530

    Updated Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 import './Styles.css';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='sidebar-container'>
       <div className='grey-box'>
         <img className='logo' src='/assets/img/kv-logo.png' />
       </div>
-      <div className='blue-box'>
+      <div className='blue-box' onClick={() => navigate(`/employees`)}>
         <div className='white'>
           <img className='people' src='/assets/img/people.png' />
         </div>
